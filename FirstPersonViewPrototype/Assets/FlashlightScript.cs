f5bef25584b86794c35e72429d88fc97a2e6e54d
loubakker <loujbakker@gmail.com>
2015-11-10 16:50:00 +0100

    FirstPersonView met flashlight in betonnen box. (Player kan nog springen)

---
+++ FirstPersonViewPrototype/Assets/FlashlightScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlightScript : MonoBehaviour {
+
+	private Light flashlight;
+
+	void Start () {
+	
+		flashlight = this.GetComponent<Light> ();
+
+	}
+	
+	void Update () {
+		
+		if (Input.GetKeyDown("f")){
+			
+			if (flashlight.enabled == true)
+			{
+				flashlight.enabled = false;
+			}
+			else
+			{
+				flashlight.enabled = true;
+			}
+			
+		}
+	}
+}
