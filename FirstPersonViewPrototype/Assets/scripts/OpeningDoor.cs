8df3111c436c299c3dc360a3bd44e99ff86c1eb6
Joost Zwart <J.H.M.Zwart@student.tudelft.nl>
2015-11-11 23:32:37 +0100

    deur toegevoegd, werkt nog niet met sleutel

---
+++ FirstPersonViewPrototype/Assets/scripts/OpeningDoor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningDoor : MonoBehaviour
+{
+
+    Animator animator;
+    bool doorOpen;
+
+    // Use this for initialization
+    void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider colli)
+    {
+        if (colli.gameObject.tag == "Player")
+        {
+            doorOpen = true;
+            Doors("Open");
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            Doors("Close");
+        }
+    }
+
+    void Doors(string state)
+    {
+        animator.SetTrigger(state);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
