06fc7cfc5111454a02d9ddd8a81c588a586102df
Dixie de Klerk <dixiedeklerk@me.com>
2015-11-13 16:06:31 +0100

    intro made by dixieeeeeeee

---
+++ Minorproject/Assets/Scripts/zoomer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zoomer : MonoBehaviour {
+
+	public int level = 1;
+	public float setTime = 1.5f;
+	public float dimTime = 2.0f;
+	public Light dimLight;
+	public float zoomSpeed = 0.01f;
+
+	Camera c;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		c = GetComponent<Camera> ();
+		timer = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		c.fieldOfView += zoomSpeed;
+
+	}
+}
