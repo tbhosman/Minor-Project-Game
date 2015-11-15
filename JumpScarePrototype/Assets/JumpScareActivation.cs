f86e409de51922df8623be8f6f1e2826aac8995d
loubakker <loujbakker@gmail.com>
2015-11-15 14:06:51 +0100

    JumpScarePrototype toegevoegd, nog niet af

---
+++ JumpScarePrototype/Assets/JumpScareActivation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpScareActivation : MonoBehaviour {
+
+	private RaycastHit RayHit;
+	private Ray FirstPersonRay;
+	public float triggerDistance;
+	private GameObject ScareObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		FirstPersonRay = new Ray (transform.position, transform.forward);
+		if (Physics.Raycast (FirstPersonRay, out RayHit, triggerDistance)){
+			if (RayHit.collider.tag == "jumpscare"){
+				ScareObject = RayHit.collider.gameObject;
+				ScareObject.GetComponent<OnActivation>().Scare = true;
+			}
+			if (RayHit.collider.tag == "VanishObject"){
+				ScareObject = RayHit.collider.gameObject;
+			}
+		}
+	}
+}
---
+++ JumpScarePrototype/Assets/VanishScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VanishScript : MonoBehaviour {
+	
+	private AudioSource audiosource;
+	public bool Trigger;
+
+	void Start () {
+		Trigger = false;
+		audiosource = GetComponent<AudioSource> ();
+		gameObject.SetActive (false);
+	}
+	
+	void Update(){
+		if (Trigger) {
+			gameObject.SetActive(true);
+		}
+	}
+}	
+
