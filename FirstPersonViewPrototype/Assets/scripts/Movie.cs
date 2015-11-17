8147bb76e48908158fdc48706846cc9eeb1d2287
Joost Zwart <J.H.M.Zwart@student.tudelft.nl>
2015-11-17 11:21:53 +0100

    tv

---
+++ FirstPersonViewPrototype/Assets/scripts/Movie.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+    [RequireComponent (typeof(AudioSource))]
+    
+public class Movie : MonoBehaviour {
+
+    public MovieTexture filmpje;
+    private AudioSource geluid;
+	void Start () {
+        GetComponent<RawImage>().texture = filmpje as MovieTexture;
+        geluid = GetComponent<AudioSource>();
+        geluid.clip = filmpje.audioClip;
+        filmpje.Play();
+        geluid.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Space)&& filmpje.isPlaying)
+        {
+            filmpje.Pause();
+        }else if (Input.GetKeyDown(KeyCode.Space) && !filmpje.isPlaying)
+        {
+            filmpje.Play();
+        }
+    }
+}
