ea6b226ce34a9326bc5ade9afd48545581c622f6
Dixie de Klerk <dixiedeklerk@me.com>
2015-11-19 14:48:39 +0100

    no message

--- Minorproject/Assets/Scripts/mainMenuButtons.cs
+++ Minorproject/Assets/Scripts/mainMenuButtons.cs
@@ -9,29 +9,20 @@ public class mainMenuButtons : MonoBehaviour {
 	private InputField input;
 	public Canvas optionsCanvas;
 	private bool infoOpen = false;
-	public GameObject quitButton;
 
 	// Use this for initialization
 	void Start () {
 		optionsCanvas.enabled = false;
-		quitButton = GameObject.Find ("Quit");
-	}
-
-	void OnMouseOver() {
-		quitButton.transform.localScale = new Vector3 (2, 2, 2);
-		//quitButton.image.rectTransform.sizeDelta = new Vector2 (200, 50); 
-		//GUILayout.BeginArea(Rect(Screen.width/4, 70, Screen.width/2, Screen.height/1.5));
-		//GUI.Button(new Rect(10, 10, 70, 30), "A button");
-
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
 	public void startGame () {
-		Application.LoadLevel ("intro");
+		Application.LoadLevel ("username");
 
 	}
 
