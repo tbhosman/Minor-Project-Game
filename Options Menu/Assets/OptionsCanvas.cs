519b469ca87f6c15718f6cb75678ab64c7ac2530
Joost Zwart <J.H.M.Zwart@student.tudelft.nl>
2015-11-19 17:05:38 +0100

    options menu in een nieuw project

---
+++ Options Menu/Assets/OptionsCanvas.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionsCanvas : MonoBehaviour {
+    public Slider aaSlider;
+    public Text aaText;
+    public Dropdown resolutionDropdown;
+	
+
+	void Start () {
+        PlayerPrefs.SetInt("AA", 4);
+        if (PlayerPrefs.HasKey("AA"))
+        {
+            aaSlider.value = Mathf.Log(PlayerPrefs.GetInt("AA"),2);
+            aaText.text = "Anti aliasing: " + PlayerPrefs.GetInt("AA") + "x";
+            if (aaSlider.value == 0)
+            {
+                aaText.text = "disabled";
+            }
+       }
+	}
+	
+    public void AAchange()
+    {   
+        PlayerPrefs.SetInt("AA",(int) Mathf.Pow(2, (int)aaSlider.value));
+        aaText.text = "Anti aliasing: " + PlayerPrefs.GetInt("AA") + "x";
+        QualitySettings.antiAliasing = PlayerPrefs.GetInt("AA");
+        if (aaSlider.value == 0)
+        {
+            aaText.text = "disabled";
+        }
+    }
+
+    public void DropDown()
+    {
+         switch (resolutionDropdown.value)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, true);
+                break;
+            case 1:
+                Screen.SetResolution(1280, 1024, true);
+                break;
+            case 2:
+                Screen.SetResolution(800, 600, true);
+                break;
+        }
+    }
+
+    void Update () {
+	
+	}
+
+    
+}
