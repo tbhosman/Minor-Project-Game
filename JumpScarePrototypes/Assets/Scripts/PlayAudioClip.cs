25a3fe650dcbba44b9cc1f5444256344b82566e1
loubakker <loujbakker@gmail.com>
2015-11-22 19:50:26 +0100

    Assets added, Scripts overzichtelijker gemaakt, JumpScares makkelijker te implementeren in een ander unity project

---
+++ JumpScarePrototypes/Assets/Scripts/PlayAudioClip.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudioClip : MonoBehaviour {
+
+	public AudioClip audioclip;
+	public float playtime;
+	public AudioSource audiosource;
+	// Use this for initialization
+	void Start () {
+		audiosource.clip = audioclip;
+		audiosource.Pause ();
+		if (playtime == 0) {
+			audiosource.Play ();
+		} else {
+			StartCoroutine (PlayAudioSource ());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator PlayAudioSource(){
+		audiosource.Play ();
+		yield return new WaitForSeconds (playtime);
+		audiosource.Stop ();
+
+	}
+}
