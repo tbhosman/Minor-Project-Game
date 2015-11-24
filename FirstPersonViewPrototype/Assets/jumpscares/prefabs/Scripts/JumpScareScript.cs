de463d2eb30b784abff81ac487d5894af90ec059
loubakker <loujbakker@gmail.com>
2015-11-24 12:28:39 +0100

    Jumpscare toegevoegd aan firstpersonviewprototype

---
+++ FirstPersonViewPrototype/Assets/jumpscares/prefabs/Scripts/JumpScareScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpScareScript : MonoBehaviour {
+	
+	private AudioSource audiosource;
+	public float scaredistance;
+	private GameObject Player;
+	public float Scarelength;
+	public bool Scare;
+	
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		GetComponent<MeshRenderer>().enabled = false;
+		Scare = false;
+		audiosource = this.GetComponent<AudioSource>();
+		audiosource.Pause ();
+	}
+	
+	void Update(){
+
+	if (Scare) {
+			if (Vector3.Distance (transform.position, Player.transform.position) < scaredistance) {
+				StartCoroutine (ShutdownAfterScareLength ());
+				Scare = false;
+			}
+		}
+	}
+
+	IEnumerator ShutdownAfterScareLength(){
+		audiosource.UnPause();
+		GetComponent<MeshRenderer>().enabled = true;
+		yield return new WaitForSeconds(Scarelength);
+		gameObject.SetActive (false);
+	}
+
+}
---
+++ JumpScarePrototypes/Assets/raycastplayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class raycastplayer : MonoBehaviour {
+	private Ray playerforwardray;
+	private RaycastHit rayhit;
+	public Vector3 rayhitposition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerforwardray = new Ray (transform.position, transform.forward);
+		if (Physics.Raycast (playerforwardray, out rayhit)) {
+			rayhitposition = rayhit.point;
+		}
+
+	
+	}
+}
