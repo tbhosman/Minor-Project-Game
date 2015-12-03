45e946de45f053ebf43e404b012db8e54fbbbc34
loubakker <loujbakker@gmail.com>
2015-12-03 15:38:29 +0100

    Prefab van hallwayscare gemaakt

---
+++ FirstPersonViewPrototype/Assets/DisableFlashlight.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableFlashlight : MonoBehaviour {
+	public GameObject flashlight;
+
+	void Start(){
+		StartCoroutine (run ());
+	}
+	IEnumerator run(){
+		flashlight.SetActive (false);
+		yield return new WaitForSeconds(0.5f);
+		flashlight.SetActive (true);
+		yield return new WaitForSeconds(0.5f);
+		flashlight.SetActive (false);
+		yield return new WaitForSeconds(0.2f);
+		flashlight.SetActive (true);
+		yield return new WaitForSeconds (0.1f);
+		flashlight.SetActive (false);
+		yield return new WaitForSeconds(0.05f);
+		flashlight.SetActive (true);
+		yield return new WaitForSeconds(0.05f);
+		flashlight.SetActive(false);
+
+	}
+}
---
+++ FirstPersonViewPrototype/Assets/ShakeCameraScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShakeCameraScript : MonoBehaviour {
+
+	public float shakeduration;
+	public float shakeIntensity;
+
+	IEnumerator Shake(){
+		
+		float elapsed = 0.0f;
+		Vector3 originalCamPos = Camera.main.transform.position;
+		
+		while (elapsed < shakeduration) {
+			
+			elapsed += Time.deltaTime;          
+			
+			float x = (Random.value * 2.0f - 1.0f)*shakeIntensity;
+			float y = (Random.value * 2.0f - 1.0f)*shakeIntensity;
+			float z = (Random.value * 2.0f - 1.0f)*shakeIntensity;
+
+			Camera.main.transform.position = new Vector3(originalCamPos.x + x, originalCamPos.y + y, originalCamPos.z + z);
+			
+			yield return null;
+		}
+		
+		Camera.main.transform.position = originalCamPos;
+	}
+}
