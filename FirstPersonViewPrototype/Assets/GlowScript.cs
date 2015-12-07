33cc2d302abd9f204a072561bbe26ea455373138
loubakker <loujbakker@gmail.com>
2015-12-07 17:09:02 +0100

    InspectNoteScript toegevoegd, kan nu naar note toelopen en oppakken. Noteglow werkt nog niet, alleen als er op de shader wordt geklikt

---
+++ FirstPersonViewPrototype/Assets/GlowScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+public class GlowScript : MonoBehaviour {
+
+	private Material mat;
+	private Renderer rend;
+	public float duration;
+	public Color fullEmissionColor;
+
+	void Start(){
+		rend = gameObject.GetComponent<Renderer> ();
+		mat = rend.material;
+		mat.EnableKeyword ("_EmissionColor");
+	}
+	void Update () {
+		float emission = Mathf.PingPong (Time.time, duration)*0.1f;
+		Color finalColor = fullEmissionColor * Mathf.LinearToGammaSpace (emission);
+		mat.SetColor ("_EmissionColor", finalColor);
+	}
+}
