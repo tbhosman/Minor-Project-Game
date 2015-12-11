2a84dfa70dd1553a79011adc52da8172e836c573
Dixie de Klerk <dixiedeklerk@me.com>
2015-12-11 14:48:38 +0100

    Script voor connectie tussen unity-server-database voor user_id

---
+++ Minorproject/Assets/Scripts/data_sending_cs.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class data_sending_cs : MonoBehaviour {
+	
+	public string url = "http://drproject.twi.tudelft.nl:8086/userid";
+	
+	// Use this for initialization
+	
+	
+	IEnumerator Start() {
+		//PlayerPrefs.SetInt ("ID", 0); //als er nog geen is aangemaakt
+		
+		
+		if (PlayerPrefs.GetInt ("ID") == 0) {
+			PlayerPrefs.SetInt ("ID", 0);
+		} 
+		
+		WWW www = new WWW (url + "?ID=" + PlayerPrefs.GetInt ("ID"));
+		yield return www;
+
+		PlayerPrefs.SetInt ("ID", www.text);
+		Debug.Log ("Player ID = " + layerPrefs.SetInt ("ID", www.text));
+
+		//		Debug.Log (www.text);
+		//		Debug.Log ("Made it");
+		
+	}
+}
