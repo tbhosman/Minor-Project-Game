a26e2f6592e12f7d0a96929c81fb1d8321a7d5a0
Tim Hosman <timhosman@hotmail.com>
2015-12-11 22:03:18 +0100

    Flag scaler gemaakt voor diverse resoluties (bij een aspect ratio kleiner dan 16:10 is het cloth-effect weggehaald i.v.m. bugs)

--- FirstPersonViewPrototype/Assets/scripts/FlagScaler.cs
+++ FirstPersonViewPrototype/Assets/scripts/FlagScaler.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class FlagScaler : MonoBehaviour {
 
+	public float AspectRatio;
+	public float scaleFactor;
+	public float camPosY;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		AspectRatio = ((float)Screen.width)/((float)Screen.height);
+		scaleFactor = (AspectRatio - 5.0f / 4.0f) * 0.3f / (16.0f / 9.0f - 5.0f / 4.0f) + 0.7f;
+		camPosY = (AspectRatio - 5.0f / 4.0f) * 8.0f / (16.0f / 9.0f - 5.0f / 4.0f) - 3.0f;
+		Vector3 flagScale = GameObject.Find("vlagGoed").transform.localScale;
+		GameObject.Find ("vlagGoed").transform.localScale = flagScale * scaleFactor;
+		Vector3 camPos = GameObject.Find ("Camera").transform.position;
+		camPos.y = camPosY;
+		GameObject.Find ("Camera").transform.position = camPos;
+		if (AspectRatio < 1.6f) {
+			gameObject.GetComponentInChildren<Cloth> ().enabled = false;
+		}
 	}
 }
