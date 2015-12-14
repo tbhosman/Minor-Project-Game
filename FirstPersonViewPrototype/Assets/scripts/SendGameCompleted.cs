b4c55b8576927d41d5ae6da2e1325ca56183fb8a
Tim Hosman <timhosman@hotmail.com>
2015-12-14 21:14:42 +0100

    Oude debug verwijderd

--- FirstPersonViewPrototype/Assets/scripts/SendGameCompleted.cs
+++ FirstPersonViewPrototype/Assets/scripts/SendGameCompleted.cs
@@ -5,7 +5,6 @@ public class SendGameCompleted : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Dit gaat goed");
 		GameObject.Find("DataAquisitie").GetComponent<DataAquisitie>().CompletedGame();
 	}
 }
