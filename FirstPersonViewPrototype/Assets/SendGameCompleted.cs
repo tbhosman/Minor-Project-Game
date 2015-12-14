e38534f21ebcd29cbe4797bc4d80c52fb9172d44
Tim Hosman <timhosman@hotmail.com>
2015-12-14 17:37:27 +0100

    Data aquisitie gedebugged, werkt nog niet helemaal

---
+++ FirstPersonViewPrototype/Assets/SendGameCompleted.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendGameCompleted : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Dit gaat goed");
+		GameObject.Find("DataAquisitie").GetComponent<DataAquisitie>().CompletedGame();
+	}
+}
--- FirstPersonViewPrototype/Assets/scripts/DataAquisitie.cs
+++ FirstPersonViewPrototype/Assets/scripts/DataAquisitie.cs
@@ -12,40 +12,59 @@ public class DataAquisitie : MonoBehaviour {
 	public int timeTaken;
 	public string url = "http://drproject.twi.tudelft.nl:8086";
 
-	void Start(){
+	IEnumerator Start(){
 		WWW www = new WWW (url + "/userid" );  //+ PlayerPrefs.GetInt ("ID"));
 		yield return www;
 		
 		if (www.isDone) {
 			int user_id = int.Parse (www.text);
-			//PlayerPrefs.SetInt ("ID", user_id); //DEZE MOET ER UIT BIJ TESTEN!
+			PlayerPrefs.SetInt ("ID", user_id); //DEZE MOET ER UIT BIJ TESTEN!
 			Debug.Log ("Player ID = " + user_id);	
 		}
 	}
 
 	public void PickedUpItem (int item) {
-		timeTaken = Mathf.RoundToInt(Time.timeSinceLevelLoad/60);
+		timeTaken = Mathf.RoundToInt (Time.timeSinceLevelLoad / 60);
 		Debug.Log ("Found item: " + item + "  on time: " + timeTaken);
+		SendPickedUpItem (item);
+	}
+
+	IEnumerator SendPickedUpItem(int item){
 		WWW www_pickUp = new WWW (url + "/pickUp" + item + "?Time=" + timeTaken + "&User_id=" + PlayerPrefs.GetInt("ID"));
 		yield return www_pickUp;
 	}
 
 	public void OpenedDoor (int door) {
-		timeTaken = Mathf.RoundToInt(Time.timeSinceLevelLoad/60);
+		timeTaken = Mathf.RoundToInt (Time.timeSinceLevelLoad / 60);
 		Debug.Log ("Opened door: " + door + "  on time: " + timeTaken);
+		SendOpenedDoor (door);
+	}
+
+	IEnumerator SendOpenedDoor (int door){
 		WWW www_door = new WWW (url + "/openedDoor" + door + "?Time=" + timeTaken + "&User_id=" + PlayerPrefs.GetInt("ID"));
 		yield return www_door;
 	}
 
 	public void CompletedGame () {
-		timeTaken = Mathf.RoundToInt(Time.timeSinceLevelLoad/60);
+		timeTaken = Mathf.RoundToInt (Time.timeSinceLevelLoad / 60);
+		Debug.Log ("Finished game in " + timeTaken + " minutes");
+		SendCompletedGame ();
+	}
+
+	IEnumerator SendCompletedGame(){
 		WWW www_gameTime = new WWW (url + "/totalGameTime?Time=" + timeTaken + "&User_id=" + PlayerPrefs.GetInt("ID"));
 		yield return www_gameTime;	
 	}
 
 	public void GameOver (Vector3 location) {
-		timeTaken = Mathf.RoundToInt(Time.timeSinceLevelLoad/60);
-		string location_string = "?Location_x=" + location.x + "?Location_y=" + location.y + "?Location_z=" + location.z;
+		timeTaken = Mathf.RoundToInt (Time.timeSinceLevelLoad / 60);
+		Debug.Log ("Lost game in " + timeTaken + " minutes");
+		SendGameOver (location);
+	}
+
+	IEnumerator SendGameOver(Vector3 location){
+		string location_string = "&Location_x=" + location.x + "&Location_y=" + location.y + "&Location_z=" + location.z;
 		WWW www_pickUp = new WWW (url + "/gameOver" + "?Time=" + timeTaken + location_string + "&User_id=" + PlayerPrefs.GetInt("ID"));
-		yield return www_pickUp;	}
+		yield return www_pickUp;
+	}
 } 
