ccf6f12889579330126150e4a112f4554de067d2
loubakker <loujbakker@gmail.com>
2015-12-15 12:22:22 +0100

    HallwayScare verbeterd, alleen nog animatie van Pim nodig

---
+++ FirstPersonViewPrototype/Assets/TogglePlayerLightScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TogglePlayerLightScript : MonoBehaviour {
+
+	public GameObject flashlight;
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player") {
+			StartCoroutine (ToggleFlashlight ());
+		}
+	}
+	IEnumerator ToggleFlashlight(){
+		flashlight.SetActive(!flashlight.activeSelf);
+		yield return new WaitForSeconds (0.5f);
+		flashlight.SetActive(!flashlight.activeSelf);
+		yield return new WaitForSeconds (0.4f);
+		flashlight.SetActive(!flashlight.activeSelf);
+		yield return new WaitForSeconds (0.3f);
+		flashlight.SetActive(!flashlight.activeSelf);
+		yield return new WaitForSeconds (0.2f);
+		flashlight.SetActive(!flashlight.activeSelf);
+		yield return new WaitForSeconds (0.1f);
+		flashlight.SetActive(!flashlight.activeSelf);
+		yield return new WaitForSeconds(0.1f);
+		flashlight.SetActive(!flashlight.activeSelf);
+		gameObject.SetActive (false);
+	}
+}
--- FirstPersonViewPrototype/Assets/scripts/SetActiveTrigger.cs
+++ FirstPersonViewPrototype/Assets/scripts/SetActiveTrigger.cs
@@ -5,13 +5,7 @@ public class SetActiveTrigger : MonoBehaviour {
 	
 	public GameObject ObjectToTrigger;
 	public bool activebool;
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+
 
 	void OnTriggerEnter(Collider col){
 		if (col.gameObject.tag == "Player") {
