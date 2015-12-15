ff38407fbde5e8da8b048540e460e3bd9e54ac58
Tim Hosman <timhosman@hotmail.com>
2015-12-15 13:08:38 +0100

    Muziek stopt nu bij het game over gaan

--- FirstPersonViewPrototype/Assets/GameOverToMenu.cs
+++ FirstPersonViewPrototype/Assets/GameOverToMenu.cs
@@ -4,9 +4,13 @@ using UnityEngine.UI;
 
 public class GameOverToMenu : MonoBehaviour {
 
+	void Start(){
+		Destroy (GameObject.Find("MainMusicController"));
+	}
+
 	// Use this for initialization
 	void Update(){
-		if (Time.timeSinceLevelLoad > 11.0f){
+		if (Time.timeSinceLevelLoad > 10.5f){
 			Application.LoadLevel("menu");
 		}
 	}
