04f0ade2a482c0262940c3da70ea9c066ed9490f
Joost Zwart <J.H.M.Zwart@student.tudelft.nl>
2015-12-15 10:55:32 +0100

    archiefmatrix schaalt mee

---
+++ FirstPersonViewPrototype/Assets/measure_width.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class measure_width : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("size: " + GetComponent<Renderer>().bounds);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
