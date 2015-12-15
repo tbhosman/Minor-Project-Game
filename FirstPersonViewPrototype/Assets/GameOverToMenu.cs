08b62e327089349734d364f46117b5b4627f6809
Tim Hosman <timhosman@hotmail.com>
2015-12-15 12:40:52 +0100

    Game over movie gaat nu over naar menu aan het eind

--- FirstPersonViewPrototype/Assets/GameOverToMenu.cs
+++ FirstPersonViewPrototype/Assets/GameOverToMenu.cs
@@ -5,16 +5,9 @@ using UnityEngine.UI;
 public class GameOverToMenu : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		waitTillEndOfMovieCall ();
-	}
-
-	private void waitTillEndOfMovieCall(){
-		StartCoroutine (waitTillEndOfMovie ());
-	}
-
-	IEnumerator waitTillEndOfMovie(){
-		yield return new WaitForSeconds (11);
-		Application.LoadLevel ("menu");
+	void Update(){
+		if (Time.timeSinceLevelLoad > 11.0f){
+			Application.LoadLevel("menu");
+		}
 	}
 }
