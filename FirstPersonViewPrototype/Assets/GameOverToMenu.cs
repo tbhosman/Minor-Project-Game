eda8164b358b2ac7a02a50d7e184d244fe784df6
Tim Hosman <timhosman@hotmail.com>
2015-12-15 12:34:54 +0100

    Game over filmpje geïmplementeerd

---
+++ FirstPersonViewPrototype/Assets/GameOverToMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverToMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		waitTillEndOfMovieCall ();
+	}
+
+	private void waitTillEndOfMovieCall(){
+		StartCoroutine (waitTillEndOfMovie ());
+	}
+
+	IEnumerator waitTillEndOfMovie(){
+		yield return new WaitForSeconds (11);
+		Application.LoadLevel ("menu");
+	}
+}
