0cde6fcc750203cf7d44405db8dcd6a30528e43c
Tim Hosman <timhosman@hotmail.com>
2015-12-15 13:20:08 +0100

    Muziek fade uit bij het verliezen van de game

--- FirstPersonViewPrototype/Assets/GameOverToMenu.cs
+++ FirstPersonViewPrototype/Assets/GameOverToMenu.cs
@@ -5,13 +5,18 @@ using UnityEngine.UI;
 public class GameOverToMenu : MonoBehaviour {
 
 	void Start(){
-		Destroy (GameObject.Find("MainMusicController"));
+		FadeOutMusic ();
 	}
 
 	// Use this for initialization
 	void Update(){
 		if (Time.timeSinceLevelLoad > 10.5f){
 			Application.LoadLevel("menu");
+			Destroy (GameObject.Find("MainMusicController"));
 		}
+	}
+
+	void FadeOutMusic(){
+		GameObject.Find ("MainMusicController").GetComponent<MainMusicController> ().FadeIn ("NietBestaandNummer"); //Alle muziek uitfaden
 	}
 }
