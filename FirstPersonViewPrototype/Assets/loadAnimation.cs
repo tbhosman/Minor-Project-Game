c6e8fd00f1694a3c77a3dbedf393e84680370a0b
Tim Hosman <timhosman@hotmail.com>
2016-01-08 09:23:40 +0100

    Geanimeerd laadscherm dat nog steeds niet correct functioneert (hij laadt nog wel)

---
+++ FirstPersonViewPrototype/Assets/loadAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadAnimation : MonoBehaviour {
+
+	public float turnSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.rotation = Quaternion.Euler(new Vector3(0,0,turnSpeed) + transform.rotation.eulerAngles);
+	}
+}
--- FirstPersonViewPrototype/Assets/scripts/LoadIntro.cs
+++ FirstPersonViewPrototype/Assets/scripts/LoadIntro.cs
@@ -1,10 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LoadIntro : MonoBehaviour {
 
+	private bool loaded;
+	private bool fadingOut;
+	private bool loading;
+	AsyncOperation async;
+
+	void Start(){
+		loaded = false;
+		fadingOut = false;
+		loading = false;
+		Application.backgroundLoadingPriority = ThreadPriority.Low;
+	}
+
 	// Use this for initialization
-	void Start () {
-		Application.LoadLevel (mainMenuButtons.leveltoload);
+	void Update() {
+		//wait for loading screen to fade in, then execute once
+		if (!GameObject.Find ("SceneFader").GetComponent<Image> ().enabled && !loaded && !loading) {
+			loading = true;
+			async = Application.LoadLevelAsync(mainMenuButtons.leveltoload);
+			//async.allowSceneActivation = false;
+			StartCoroutine (LoadLevel (async));
+		}
+
+		//if next scene is loaded, start fading out loading screen
+		if (loaded) {
+			GameObject.Find ("SceneFader").GetComponent<SceneFadeInOut> ().FadeToBlack();
+			fadingOut = true;
+		}
+
+		//when faded out, switch to new scene
+		if (GameObject.Find ("SceneFader").GetComponent<Image> ().color.a >= 0.95f && loaded) {
+			async.allowSceneActivation = true;
+		}
+	}
+
+	IEnumerator LoadLevel(AsyncOperation async){
+		while (!async.isDone) {
+			yield return async;
+		}
+		Debug.Log("Loading complete");
+		loaded = true;
 	}
 }
