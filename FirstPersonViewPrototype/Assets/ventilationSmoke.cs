daf920781427a979632ecd8abf859670f2240754
Joost Zwart <J.H.M.Zwart@student.tudelft.nl>
2016-01-12 15:03:35 +0100

    geluid voor stoom

--- FirstPersonViewPrototype/Assets/ventilationSmoke.cs
+++ FirstPersonViewPrototype/Assets/ventilationSmoke.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class ventilationSmoke : MonoBehaviour {
     public ParticleSystem ventilatie;
+    public AudioClip audioclip;
+    public float playtime;
+    public AudioSource audiosource;
+    public float playfromseconds;
+    public bool hasaudio;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Rook();
         InvokeRepeating("Rook", 3, 3);
     }
@@ -15,6 +20,23 @@ public class ventilationSmoke : MonoBehaviour {
         if (Random.Range(1, 5) <= 1)
         {
             ventilatie.Play();
+            
+                audiosource.clip = audioclip;
+                audiosource.time = playfromseconds;
+                audiosource.Play();
+                StartCoroutine(PlayAudioSource());
         }
     }
+
+    IEnumerator PlayAudioSource()
+    {
+        audiosource.Play();
+        yield return new WaitForSeconds(playtime);
+        audiosource.Stop();
+        audiosource.time = 0;
+    }
 }
+
+
+
+
