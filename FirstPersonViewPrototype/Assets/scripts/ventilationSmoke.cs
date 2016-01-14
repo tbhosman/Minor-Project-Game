e4f3983d94ee055f6bf7baa39254b45a3a38ea34
Tim Hosman <timhosman@hotmail.com>
2016-01-14 11:26:32 +0100

    Extra merge fix

--- FirstPersonViewPrototype/Assets/scripts/ventilationSmoke.cs
+++ FirstPersonViewPrototype/Assets/scripts/ventilationSmoke.cs
@@ -1,15 +1,16 @@
-﻿/// <summary>
-/// Randomly starts a smoke effect through the ventilation
-/// </summary>
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class ventilationSmoke : MonoBehaviour {
     public ParticleSystem ventilatie;
+    public AudioClip audioclip;
+    public float playtime;
+    public AudioSource audiosource;
+    public float playfromseconds;
+    public bool hasaudio;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Rook();
         InvokeRepeating("Rook", 3, 3);
     }
@@ -19,6 +20,23 @@ public class ventilationSmoke : MonoBehaviour {
         if (Random.Range(1, 5) <= 1)
         {
             ventilatie.Play();
+            
+                audiosource.clip = audioclip;
+                audiosource.time = playfromseconds;
+                audiosource.Play();
+                StartCoroutine(PlayAudioSource());
         }
     }
+
+    IEnumerator PlayAudioSource()
+    {
+        audiosource.Play();
+        yield return new WaitForSeconds(playtime);
+        audiosource.Stop();
+        audiosource.time = 0;
+    }
 }
+
+
+
+
