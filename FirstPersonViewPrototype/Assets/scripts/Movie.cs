34f3c1604840dc7f43066ca075985fb38d1e1473
Tim Hosman <timhosman@hotmail.com>
2016-01-14 14:22:01 +0100

    Volume rolloff werkt nu ook voor de TV

--- FirstPersonViewPrototype/Assets/scripts/Movie.cs
+++ FirstPersonViewPrototype/Assets/scripts/Movie.cs
@@ -12,7 +12,12 @@ public class Movie : MonoBehaviour {
 
     public MovieTexture filmpje;
     private AudioSource geluid;
+	private GameObject player;
+	public float maxVolume = 0.1f;
+	public float maxDistance = 10.0f;
+
 	void Start () {
+		player = GameObject.Find ("FPSController");
         GetComponent<RawImage>().texture = filmpje as MovieTexture;
         geluid = GetComponent<AudioSource>();
         geluid.clip = filmpje.audioClip;
@@ -22,12 +27,22 @@ public class Movie : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if(Input.GetKeyDown(KeyCode.Space)&& filmpje.isPlaying)
-        {
-            filmpje.Pause();
-        }else if (Input.GetKeyDown(KeyCode.Space) && !filmpje.isPlaying)
-        {
-            filmpje.Play();
-        }
+
+		float distance = Vector3.Distance (player.transform.position, transform.position);
+
+		// manual volume rolloff, because this is bugged for movietextures: 
+		///http://issuetracker.unity3d.com/issues/movietexture-rolloff-not-working-on-movie-textures-audio
+		if (distance > maxDistance) {
+			geluid.volume = 0.0f;
+		} else {
+			geluid.volume = maxVolume - maxVolume / maxDistance * distance;
+		}
+//	    if(Input.GetKeyDown(KeyCode.Space)&& filmpje.isPlaying)
+//        {
+//            filmpje.Pause();
+//        }else if (Input.GetKeyDown(KeyCode.Space) && !filmpje.isPlaying)
+//        {
+//            filmpje.Play();
+//        }
     }
 }
